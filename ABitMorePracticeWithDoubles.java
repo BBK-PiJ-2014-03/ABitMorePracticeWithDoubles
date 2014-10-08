4bd1e7ef4d006a93dfee2b61cb4e9e8553676f9a
igalna <igalna@gmail.com>
2014-10-08 16:37:32 +0100

    Second Commit

--- ABitMorePracticeWithDoubles.java
+++ ABitMorePracticeWithDoubles.java
@@ -2,14 +2,52 @@ import java.util.Scanner;
 
 public class ABitMorePracticeWithDoubles {
 
+    double total;
+    double years;
+    double interest;
+    
+    Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
     
-        Scanner keyboard = new Scanner(System.in);
         
-        double total;
-        double years;
-        double interest;
         
+        ABitMorePracticeWithDoubles test = new ABitMorePracticeWithDoubles();
+    
+        test.findInfo();
+        
+        System.out.println("Your total to be repaid is " + test.toBePaid());
+        System.out.println("You amount to be paid each year is " + test.everyYear());
+        
+        
+        System.out.println("The number of years before you are paying off " + 
+                         "principal is " + (test.calcInterest() / test.everyYear()));
+    
+    }
+
+    public double toBePaid() {
+    
+        return calcInterest() + total;
+    }
+    
+    public double everyYear() {
+        
+        return calcInterest() + (total/years);
+    }
+    
+    
+    public double calcInterest() {
+        
+        double compound = 0;
+        
+        for (int count = 0; count < years; count++) {
+            compound += total * interest;
+        }
+        
+        return compound;
+    }
+    
+    public void findInfo() {
         System.out.print("How much did you borrow in total? > ");
         total = keyboard.nextDouble();
         
@@ -18,34 +56,5 @@ public class ABitMorePracticeWithDoubles {
         
         System.out.print("What is the interest rate? > ");
         interest = keyboard.nextDouble();
-        
     }
-
-    public double toBePaid(double total, double years, double interest) {
-        
-        double compound = 0;
-        
-        for (int count = 0; count < this.years; count++) {
-            compound += this.total * this.interest;
-        }
-        
-        return compound + this.total;
-    }
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+}
